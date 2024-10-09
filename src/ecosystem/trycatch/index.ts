99aa9408f659a99946dd1aa24d39ac05050c70c8
cudenixdeveloper <developer@cudenix.com>
2024-10-09 00:12:28 -0300

    Add TryCatch middleware module

---
+++ src/ecosystem/trycatch/index.ts
@@ -0,0 +1,1 @@
+export * from "@/ecosystem/trycatch/trycatch";
---
+++ src/ecosystem/trycatch/trycatch.ts
@@ -0,0 +1,14 @@
+import { error } from "@/error";
+import { module } from "@/module";
+
+export const trycatch = {
+	module() {
+		return module().middleware(async (_context, next) => {
+			try {
+				return await next();
+			} catch (exception) {
+				return error(exception);
+			}
+		});
+	},
+};
