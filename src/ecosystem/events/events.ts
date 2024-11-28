b35a8a8cf1e846245ead243b97231c96b779f838
cudenixdeveloper <developer@cudenix.com>
2024-11-27 22:17:03 -0300

    Refactor Event type definition and simplify Event constructor implementation

--- src/ecosystem/events/events.ts
+++ src/ecosystem/events/events.ts
@@ -2,17 +2,15 @@ import { EventEmitter } from "node:events";
 
 import type { EventsList } from "@cudenix/cudenix/events";
 
-export interface Event<Events extends Record<keyof Events, unknown[]>>
-	extends EventEmitter<Events> {}
+export type Event<Events extends Record<keyof Events, unknown[]>> =
+	EventEmitter<Events>;
 
 export type AnyEvent = Event<any>;
 
 type Constructor = new () => AnyEvent;
 
 export const Event = function (this: AnyEvent) {
-	const eventEmitter = new EventEmitter();
-
-	return eventEmitter;
+	return new EventEmitter();
 } as unknown as Constructor;
 
 Event.prototype = Object.create(EventEmitter.prototype);
