c0c40ca66038bc9f0f2a6827aa4c6f30a38136b2
cudenixdeveloper <developer@cudenix.com>
2025-02-08 00:43:41 -0300

    Refactor trycatch

--- src/ecosystem/trycatch/trycatch.ts
+++ src/ecosystem/trycatch/trycatch.ts
@@ -1,14 +1,16 @@
 import { error } from "@/error";
-import { module } from "@/module";
+import { module as _module } from "@/module";
+
+const module = () => {
+	return _module().middleware(async (_context, next) => {
+		try {
+			return await next();
+		} catch (exception) {
+			return error(exception || "An unknown error has occurred", 500);
+		}
+	});
+};
 
 export const trycatch = {
-	module() {
-		return module().middleware(async (_context, next) => {
-			try {
-				return await next();
-			} catch (exception) {
-				return error(exception || "An unknown error has occurred", 500);
-			}
-		});
-	},
+	module,
 };
