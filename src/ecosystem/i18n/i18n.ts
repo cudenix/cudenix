73a1cd4d5159aa87dc8aefa468565f58887aff92
cudenixdeveloper <developer@cudenix.com>
2025-02-09 23:44:32 -0300

    Refactor i18n

--- src/ecosystem/i18n/i18n.ts
+++ src/ecosystem/i18n/i18n.ts
@@ -1,4 +1,5 @@
 import { readdir } from "node:fs/promises";
+import { join } from "node:path";
 import { module as _module } from "@/module";
 
 import type { App } from "@/app";
@@ -9,38 +10,40 @@ import { getCookies } from "@/utils/get-cookies";
 type DeepPaths<Type extends Record<PropertyKey, unknown>> = {
 	[Key in keyof Type]: Key extends string
 		? Type[Key] extends Record<PropertyKey, unknown>
-			? [Key] | [Key, ...DeepPaths<Type[Key]>]
-			: [Key]
+			? `${Key}` | `${Key}.${DeepPaths<Type[Key]>}`
+			: `${Key}`
 		: never;
 }[keyof Type];
 
 type DeepValue<
 	Type extends Record<PropertyKey, unknown>,
-	Path extends string[],
-> = Path extends [infer Key, ...infer Rest]
+	Path extends string,
+> = Path extends `${infer Key}.${infer Rest}`
 	? Key extends keyof Type
-		? Rest extends [string, ...string[]]
-			? Type[Key] extends Record<PropertyKey, unknown>
-				? DeepValue<Type[Key], Rest>
-				: never
-			: Type[Key]
+		? Type[Key] extends Record<PropertyKey, unknown>
+			? DeepValue<Type[Key], Rest>
+			: never
 		: never
-	: never;
+	: Path extends keyof Type
+		? Type[Path]
+		: never;
 
 type ExtractPlaceholders<String extends string> =
 	String extends `${infer _Start}\${${infer Param}}${infer Rest}`
 		? Param | ExtractPlaceholders<Rest>
 		: never;
 
-interface TranslateOptions<Key extends string> {
-	language?: string;
-	replace?: {
-		[_Key in ExtractPlaceholders<Key>]?: string;
-	};
-}
-
 interface Translation {
 	[key: string]: string | Translation | (string | Translation)[];
+}
+
+interface TranslateOptions<Translation> {
+	language?: string;
+	replace?: Translation extends string
+		? {
+				[Key in ExtractPlaceholders<Translation>]?: string;
+			}
+		: undefined;
 }
 
 interface I18n {
@@ -53,6 +56,33 @@ interface I18n {
 }
 
 type I18nAddonOptions = Pick<I18n, "cookie" | "header">;
+
+const loadTranslations = async (directory: string) => {
+	const result = {} as Translation;
+	const entries = await readdir(directory, {
+		withFileTypes: true,
+	});
+
+	for (const entry of entries) {
+		const fullPath = join(directory, entry.name);
+
+		if (entry.isDirectory()) {
+			result[entry.name] = await loadTranslations(fullPath);
+		} else if (entry.isFile() && entry.name.endsWith(".json")) {
+			const data = await Bun.file(fullPath).json();
+
+			if (entry.name === "index.json") {
+				Object.assign(result, data);
+			} else {
+				const key = entry.name.slice(0, -5);
+
+				result[key] = data;
+			}
+		}
+	}
+
+	return result;
+};
 
 const addon = (path: string, language: string, options?: I18nAddonOptions) => {
 	return async function (this: App) {
@@ -71,16 +101,19 @@ const addon = (path: string, language: string, options?: I18nAddonOptions) => {
 			translations: new Empty() as Translation,
 		});
 
+		const i18n = this.memory.get("i18n") as I18n;
+
 		for (let i = 0; i < languages.length; i++) {
-			(this.memory.get("i18n") as I18n).translations[languages[i]] =
-				await Bun.file(
-					`${path}/${languages[i]}/${languages[i]}.json`,
-				).json();
+			const lang = languages[i];
+
+			i18n.translations[lang] = await loadTranslations(join(path, lang));
 		}
 
 		await Bun.write(
-			`${path}/types.d.ts`,
-			`namespace Cudenix.i18n { interface Translations ${JSON.stringify((this.memory.get("i18n") as I18n).translations[language])}; };`,
+			join(path, "types.d.ts"),
+			`namespace Cudenix.i18n { interface Translations ${JSON.stringify(
+				i18n.translations[language],
+			)}; };`,
 		);
 
 		return "i18n";
@@ -120,34 +153,30 @@ const module = () => {
 	);
 };
 
-function translate<const Path extends DeepPaths<Cudenix.i18n.Translations>>(
-	...path: Path
-): DeepValue<Cudenix.i18n.Translations, Path>;
+const replace = <Translation extends string>(
+	translation: Translation,
+	replace: {
+		[Key in ExtractPlaceholders<Translation>]?: string;
+	},
+): Translation => {
+	const keys = Object.keys(replace);
 
-function translate<const Path extends DeepPaths<Cudenix.i18n.Translations>>(
-	...args: [
-		...path: Path,
-		options: TranslateOptions<DeepValue<Cudenix.i18n.Translations, Path>>,
-	]
-): DeepValue<Cudenix.i18n.Translations, Path>;
+	let replaced = translation as string;
 
-function translate(...args: any[]): any {
-	let options: TranslateOptions<string> | undefined;
-	let path: string[];
+	for (let i = 0; i < keys.length; i++) {
+		const key = keys[i];
 
-	if (
-		args.length > 0 &&
-		typeof args[args.length - 1] === "object" &&
-		args[args.length - 1] !== null &&
-		!Array.isArray(args[args.length - 1])
-	) {
-		options = args.pop();
-
-		path = args;
-	} else {
-		path = args;
+		// @ts-expect-error
+		replaced = replaced.replaceAll(`\${${key}}`, replace[key]);
 	}
 
+	return replaced as Translation;
+};
+
+const translate = <const Path extends DeepPaths<Cudenix.i18n.Translations>>(
+	path: Path,
+	options?: TranslateOptions<DeepValue<Cudenix.i18n.Translations, Path>>,
+): DeepValue<Cudenix.i18n.Translations, Path> => {
 	const context = getRequestContext();
 
 	const translations = (context?.memory.get("i18n") as I18n | undefined)
@@ -158,13 +187,15 @@ function translate(...args: any[]): any {
 	];
 
 	if (!translations) {
-		return path.join(".");
+		return path as DeepValue<Cudenix.i18n.Translations, Path>;
 	}
+
+	const split = path.split(".");
 
 	let translation = translations as Translation[string];
 
-	for (let i = 0; i < path.length; i++) {
-		translation = (translation as Translation)[path[i]];
+	for (let i = 0; i < split.length; i++) {
+		translation = (translation as Translation)[split[i]];
 	}
 
 	if (options?.replace) {
@@ -180,8 +211,8 @@ function translate(...args: any[]): any {
 		}
 	}
 
-	return translation;
-}
+	return translation as DeepValue<Cudenix.i18n.Translations, Path>;
+};
 
 export const i18n = {
 	addon,
@@ -195,6 +226,8 @@ export const i18n = {
 	},
 
 	module,
+
+	replace,
 
 	translate,
 };
