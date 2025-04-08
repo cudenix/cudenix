8c43b3db819e269f1a3c2c040c8aabad93ee5e9c
cudenixdeveloper <developer@cudenix.com>
2025-04-08 04:18:05 -0300

    Refactor cluster function to use Bun for spawning processes and remove unused cluster module

--- src/ecosystem/cluster/cluster.ts
+++ src/ecosystem/cluster/cluster.ts
@@ -1,38 +1,33 @@
-import _cluster from "node:cluster";
-import { cpus } from "node:os";
-
-export const cluster = async (
-	app: () => unknown | Promise<unknown>,
-): Promise<typeof _cluster> => {
-	let sigint = false;
-
-	process.on("SIGINT", () => {
-		sigint = true;
-
-		const keys = Object.keys(_cluster.workers ?? {});
-
-		for (let i = 0; i < keys.length; i++) {
-			_cluster.workers?.[keys[i]]?.kill();
-		}
-
-		process.exit(0);
-	});
-
-	if (_cluster.isPrimary) {
-		for (let i = 0; i < cpus().length; i++) {
-			_cluster.fork();
-		}
-
-		_cluster.on("exit", () => {
-			if (sigint) {
-				return;
-			}
-
-			_cluster.fork();
-		});
-	} else {
-		await app();
+export const cluster = (command: string) => {
+	if (process.env.WORKER_ID) {
+		return;
 	}
 
-	return _cluster;
+	const cpus = navigator.hardwareConcurrency;
+	const buns = new Array(cpus);
+
+	for (let i = 0; i < cpus; i++) {
+		buns[i] = Bun.spawn({
+			cmd: command.split(" "),
+			env: {
+				...process.env,
+				WORKER_ID: (i + 1).toString(),
+			},
+			stderr: "inherit",
+			stdin: "inherit",
+			stdout: "inherit",
+		});
+	}
+
+	const kill = () => {
+		for (let i = 0; i < cpus; i++) {
+			try {
+				buns[i].kill();
+			} catch {}
+		}
+	};
+
+	process.on("SIGINT", kill);
+
+	process.on("exit", kill);
 };
