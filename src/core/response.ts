24517a50123e9a108a2eb867377b2eed3cffb731
cudenixdeveloper <developer@cudenix.com>
2025-10-21 06:11:15 -0300

    Remove iteration handling for response content to simplify processResponse function

--- src/core/response.ts
+++ src/core/response.ts
@@ -1,27 +1,8 @@
 import type { ContextResponse } from "@/core";
 
-const MAX_ITERATIONS = 10;
-
 export const processResponse = async (
 	response: ContextResponse,
 ): Promise<Response> => {
-	let iterations = 0;
-
-	while (
-		typeof response.content === "function" ||
-		response.content instanceof Promise
-	) {
-		if (iterations++ > MAX_ITERATIONS) {
-			break;
-		}
-
-		if (typeof response.content === "function") {
-			response.content = (response.content as () => any)();
-		} else {
-			response.content = await response.content;
-		}
-	}
-
 	if (response.content instanceof ReadableStream) {
 		response.headers.set("Cache-Control", "no-cache");
 		response.headers.set("Connection", "keep-alive");
