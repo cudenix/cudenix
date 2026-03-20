dd1c67eb17610ea4d39c2cef84ab05720dfc325c
cudenixdeveloper <developer@cudenix.com>
2026-03-20 03:26:00 -0400

    refactor: Enhance InferValidatorError type to handle optional '~types' property

--- src/core/global.ts
+++ src/core/global.ts
@@ -3,7 +3,15 @@ import type { StandardSchemaV1 } from "@/types/standard-schema";
 declare global {
 	namespace Cudenix {
 		type InferValidatorError<Type> = Type extends StandardSchemaV1
-			? StandardSchemaV1.Issue[]
+			? Type extends {
+					"~types"?: unknown;
+				}
+				? NonNullable<Type["~types"]> extends {
+						issue: infer Issue;
+					}
+					? Issue
+					: StandardSchemaV1.Issue[]
+				: StandardSchemaV1.Issue[]
 			: Type;
 
 		type InferValidatorInput<Type> = Type extends StandardSchemaV1
