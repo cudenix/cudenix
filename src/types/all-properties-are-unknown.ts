8a1f295c65d9989403b0590ab4e11fd7108e0d12
cudenixdeveloper <developer@cudenix.com>
2026-03-23 01:55:45 -0500

    refactor: Simplify AllPropertiesAreUnknown type definition by removing unnecessary checks

--- src/types/all-properties-are-unknown.ts
+++ src/types/all-properties-are-unknown.ts
@@ -1,9 +1,5 @@
 export type AllPropertiesAreUnknown<Type> = {
-	[Key in keyof Type]: Type[Key] extends unknown
-		? unknown extends Type[Key]
-			? true
-			: false
-		: false;
+	[Key in keyof Type]: unknown extends Type[Key] ? true : false;
 }[keyof Type] extends true | undefined
 	? true
 	: false;
