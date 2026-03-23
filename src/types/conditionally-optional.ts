5c1d35c880ae4e5e8757227f1ca619feb3ed1861
cudenixdeveloper <developer@cudenix.com>
2026-03-23 01:55:55 -0500

    refactor: Simplify ConditionallyOptional type by removing OptionalKeys and using direct conditional logic

--- src/types/conditionally-optional.ts
+++ src/types/conditionally-optional.ts
@@ -1,14 +1,12 @@
-type OptionalKeys<
-	Type extends Record<PropertyKey, unknown>,
-	OptionalType,
-> = keyof {
-	[Key in keyof Type as OptionalType extends Type[Key]
-		? Key
-		: never]: Type[Key];
-};
-
 export type ConditionallyOptional<
 	Type extends Record<PropertyKey, unknown>,
 	OptionalType,
-> = Omit<Type, OptionalKeys<Type, OptionalType>> &
-	Partial<Pick<Type, OptionalKeys<Type, OptionalType>>>;
+> = {
+	[Key in keyof Type as OptionalType extends Type[Key]
+		? never
+		: Key]: Type[Key];
+} & {
+	[Key in keyof Type as OptionalType extends Type[Key]
+		? Key
+		: never]?: Type[Key];
+};
