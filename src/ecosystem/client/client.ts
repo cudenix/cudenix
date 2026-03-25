5e2e7016a6f854941624bb22c69ec26e25eec106
cudenixdeveloper <developer@cudenix.com>
2026-03-25 12:50:05 -0300

    refactor: Simplify ParseResponse type by consolidating response condition checks

--- src/ecosystem/client/client.ts
+++ src/ecosystem/client/client.ts
@@ -21,9 +21,7 @@ export type RequestOptions<Request> = Merge<
 
 export type ParseResponse<Method, Request, Response> = Method extends "WS"
 	? WS<Request, Response>
-	: Response extends
-			| Generator<infer Yield>
-			| AsyncGenerator<infer Yield>
+	: Response extends Generator<infer Yield> | AsyncGenerator<infer Yield>
 		? SSE<Yield extends AnyGeneratorSSE ? Yield : never>
 		: Response extends AnyError | AnySuccess
 			? Response["transform"] extends true
