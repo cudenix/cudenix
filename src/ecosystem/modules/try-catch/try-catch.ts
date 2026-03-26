ab124482bfdfee3fbf0fa20e306fdfc8d46f973b
cudenixdeveloper <developer@cudenix.com>
2026-03-25 23:05:13 -0300

    refactor: Rename trycatch module to try-catch and enhance error handling with debug option

--- src/ecosystem/modules/try-catch/try-catch.ts
+++ src/ecosystem/modules/try-catch/try-catch.ts
@@ -1,11 +1,19 @@
 import { error } from "@/core/error";
 import { module } from "@/core/module";
 
-export const trycatch = () => {
+export interface TryCatchOptions {
+	debug?: boolean;
+}
+
+export const tryCatch = ({ debug = false }: TryCatchOptions = {}) => {
 	return module().middleware(async (context, next) => {
 		try {
 			await next();
 		} catch (exception) {
+			if (debug) {
+				console.error(exception);
+			}
+
 			return error(exception || "An unknown error has occurred.", {
 				status: 500,
 			});
