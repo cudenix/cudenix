01a1cd0a3182d70e2659063acec74a1eb53f46ad
cudenixdeveloper <developer@cudenix.com>
2026-03-26 20:55:13 -0300

    refactor: Use FreezeEmpty as default value for TryCatchOptions

--- src/ecosystem/plugins/try-catch/try-catch.ts
+++ src/ecosystem/plugins/try-catch/try-catch.ts
@@ -1,11 +1,12 @@
 import { error } from "@/core/error";
 import { module } from "@/core/module";
+import { FreezeEmpty } from "@/utils/objects/empty";
 
 export interface TryCatchOptions {
 	debug?: boolean;
 }
 
-export const tryCatch = ({ debug = false }: TryCatchOptions = {}) => {
+export const tryCatch = ({ debug = false }: TryCatchOptions = FreezeEmpty) => {
 	return module().middleware(async (context, next) => {
 		try {
 			await next();
