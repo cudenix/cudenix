9fa287a97cf17dba444eb8c44f8f5e7e4d72edcd
cudenixdeveloper <developer@cudenix.com>
2026-03-26 19:45:21 -0300

    refactor: Replace regex with replaceAll for improved HTML escaping in scalar function

--- src/ecosystem/plugins/openapi/scalar.ts
+++ src/ecosystem/plugins/openapi/scalar.ts
@@ -1,5 +1,3 @@
-const CLOSE_TAG_REGEXP = /</g;
-
 export const scalar = (title: string, spec: string, configuration: string) => {
 	return `
         <!DOCTYPE html>
@@ -13,11 +11,11 @@ export const scalar = (title: string, spec: string, configuration: string) => {
 
             <body>
                 <script id="api-reference" type="application/json">
-                    ${spec.replace(CLOSE_TAG_REGEXP, "\\u003c")}
+                    ${spec.replaceAll("<", "\\u003c")}
                 </script>
 
                 <script>
-                    document.getElementById("api-reference").dataset.configuration = '${configuration.replace(CLOSE_TAG_REGEXP, "\\u003c").replace(/'/g, "\\u0027")}';
+                    document.getElementById("api-reference").dataset.configuration = '${configuration.replaceAll("<", "\\u003c").replaceAll("'", "\\u0027")}';
                 </script>
 
                 <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
