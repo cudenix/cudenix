cea35b738439fd820840c3214efe8d644605c705
cudenixdeveloper <developer@cudenix.com>
2026-04-01 02:07:17 -0300

    refactor: Rename asyncLocalStorage to ASYNC_LOCAL_STORAGE for consistency

--- src/ecosystem/plugins/global-request-context/global-request-context.ts
+++ src/ecosystem/plugins/global-request-context/global-request-context.ts
@@ -3,14 +3,14 @@ import { AsyncLocalStorage } from "node:async_hooks";
 import type { AnyDeveloperContext } from "@/core/context";
 import { module } from "@/core/module";
 
-export const asyncLocalStorage = new AsyncLocalStorage<AnyDeveloperContext>();
+export const ASYNC_LOCAL_STORAGE = new AsyncLocalStorage<AnyDeveloperContext>();
 
 export const getRequestContext = () => {
-	return asyncLocalStorage.getStore();
+	return ASYNC_LOCAL_STORAGE.getStore();
 };
 
 export const globalRequestContext = () => {
 	return module().middleware(async (context, next) => {
-		await asyncLocalStorage.run(context, next);
+		await ASYNC_LOCAL_STORAGE.run(context, next);
 	});
 };
