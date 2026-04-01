e77290271085eae6ca07488795f0fff4f799c414
cudenixdeveloper <developer@cudenix.com>
2026-04-01 02:07:13 -0300

    refactor: Simplify title and configuration handling in scalar function

--- src/ecosystem/plugins/openapi/scalar.ts
+++ src/ecosystem/plugins/openapi/scalar.ts
@@ -3,7 +3,7 @@ export const scalar = (title: string, spec: string, configuration: string) => {
         <!DOCTYPE html>
         <html>
             <head>
-                <title>${Bun.escapeHTML(title)}</title>
+                <title>${title}</title>
 
                 <meta charset="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -11,11 +11,11 @@ export const scalar = (title: string, spec: string, configuration: string) => {
 
             <body>
                 <script id="api-reference" type="application/json">
-                    ${spec.replaceAll("<", "\\u003c")}
+                    ${spec}
                 </script>
 
                 <script>
-                    document.getElementById("api-reference").dataset.configuration = '${configuration.replaceAll("<", "\\u003c").replaceAll("'", "\\u0027")}';
+                    document.getElementById("api-reference").dataset.configuration = '${configuration}';
                 </script>
 
                 <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
