8b51ac0b88cec806e164d31b6a3926f4b38ccf3a
cudenixdeveloper <developer@cudenix.com>
2026-04-14 17:07:37 -0300

    refactor: remove unnecessary comment from Empty constructor

--- src/utils/objects/empty.ts
+++ src/utils/objects/empty.ts
@@ -1,4 +1,3 @@
-// Biome-ignore lint/complexity/useArrowFunction: Is necessary for the prototype
 export const Empty = function Empty() {} as unknown as new () => Record<PropertyKey, unknown>;
 
 Empty.prototype = Object.create(null);
