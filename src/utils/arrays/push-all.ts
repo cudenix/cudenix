c6c4a35986bda1f7161de90610b46fb32bcfcbf1
cudenixdeveloper <developer@cudenix.com>
2026-05-14 20:57:40 -0300

    refactor: simplify pushAll function by removing unnecessary length adjustment and using direct indexing

--- src/utils/arrays/push-all.ts
+++ src/utils/arrays/push-all.ts
@@ -2,11 +2,7 @@ export const pushAll = <Type>(target: Type[], source: Type[]) => {
 	const baseLength = target.length;
 	const sourceLength = source.length;
 
-	target.length = baseLength + sourceLength;
-
-	let j = baseLength;
-
 	for (let i = 0; i < sourceLength; i++) {
-		target[j++] = source[i]!;
+		target[baseLength + i] = source[i]!;
 	}
 };
