c65464ba6f2ab7f6c4e2066e525571c95e9996c7
cudenixdeveloper <developer@cudenix.com>
2026-05-14 03:05:35 -0300

    refactor: streamline merge function by using for...in loop and removing unnecessary checks

--- src/utils/objects/merge.ts
+++ src/utils/objects/merge.ts
@@ -2,17 +2,7 @@ export const merge = (
 	object1: Record<PropertyKey, unknown>,
 	object2: Record<PropertyKey, unknown>,
 ) => {
-	const keys = Object.keys(object2);
-
-	for (let i = 0; i < keys.length; i++) {
-		const key = keys[i];
-
-		if (key === undefined) {
-			continue;
-		}
-
+	for (const key in object2) {
 		object1[key] = object2[key];
 	}
-
-	return object1;
 };
