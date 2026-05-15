dc28797ffc5b9d49f5952e46e07c0792cc4871cc
cudenixdeveloper <developer@cudenix.com>
2026-05-15 00:28:50 -0300

    refactor: simplify ConditionallyOptional type definition for improved clarity and correctness

--- src/types/conditionally-optional.ts
+++ src/types/conditionally-optional.ts
@@ -2,11 +2,7 @@ export type ConditionallyOptional<
 	Type extends Record<PropertyKey, unknown>,
 	OptionalType,
 > = {
-	[Key in keyof Type as OptionalType extends Type[Key]
-		? never
-		: Key]: Type[Key];
-} & {
-	[Key in keyof Type as OptionalType extends Type[Key]
-		? Key
-		: never]?: Type[Key];
-};
+	[Key in keyof Type]: OptionalType extends Type[Key] ? Key : never;
+}[keyof Type] extends infer OptionalKey extends keyof Type
+	? Omit<Type, OptionalKey> & { [Key in OptionalKey]?: Type[Key] }
+	: never;
