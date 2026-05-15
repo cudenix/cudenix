0775cf1556593110d9093c02ba8d6e560abdfe3d
cudenixdeveloper <developer@cudenix.com>
2026-05-15 00:27:42 -0300

    refactor: update MaybeFunction type definition to allow direct Promise<Type> return

--- src/types/maybe-function.ts
+++ src/types/maybe-function.ts
@@ -1,3 +1,1 @@
-import type { MaybePromise } from "@/types/maybe-promise";
-
-export type MaybeFunction<Type> = Type | (() => MaybePromise<Type>);
+export type MaybeFunction<Type> = Type | (() => Type | Promise<Type>);
