a245157d420790c00a8b087c4a2e6be8f0a1e2c7
cudenixdeveloper <developer@cudenix.com>
2026-05-15 00:29:03 -0300

    refactor: format ConditionallyOptional type definition for improved readability

--- src/types/conditionally-optional.ts
+++ src/types/conditionally-optional.ts
@@ -4,5 +4,7 @@ export type ConditionallyOptional<
 > = {
 	[Key in keyof Type]: OptionalType extends Type[Key] ? Key : never;
 }[keyof Type] extends infer OptionalKey extends keyof Type
-	? Omit<Type, OptionalKey> & { [Key in OptionalKey]?: Type[Key] }
+	? Omit<Type, OptionalKey> & {
+			[Key in OptionalKey]?: Type[Key];
+		}
 	: never;
