d05d44e2cbe2d387556f03926ce1c5c7f1510e01
cudenixdeveloper <developer@cudenix.com>
2026-05-27 01:05:40 -0300

    refactor: update endpoint handling to use Record instead of Map and improve type consistency

--- src/core/compile.ts
+++ src/core/compile.ts
@@ -1,7 +1,5 @@
-import { getLinkInfo, USE_ALL } from "@/core/analyzer";
-import type { App, Chain, Endpoint } from "@/core/app";
+import type { Chain, Cudenix, Endpoint } from "@/core/cudenix";
 import { compileEndpointFetch } from "@/core/jit";
-import { memoizeRequest } from "@/core/memoize";
 import { type AnyModule, Module } from "@/core/module";
 import { cloneAppend } from "@/utils/arrays/clone-append";
 import { pushAll } from "@/utils/arrays/push-all";
@@ -15,7 +13,7 @@ interface PreviousStep {
 }
 
 const step = (
-	endpoints: Map<string, Endpoint[]>,
+	endpoints: Record<string, Endpoint[]>,
 	module: AnyModule,
 	previous: PreviousStep,
 ) => {
@@ -39,11 +37,7 @@ const step = (
 
 			module.chain = merged.slice();
 
-			step(endpoints, link.group(module), {
-				bits,
-				chain: [],
-				path: "",
-			});
+			step(endpoints, link.group(module), { bits, chain: [], path: "" });
 
 			continue;
 		}
@@ -80,12 +74,12 @@ const step = (
 
 		const method = link.method === "WS" ? "GET" : link.method;
 
-		let methodEndpoints = endpoints.get(method);
+		let methodEndpoints = endpoints[method];
 
 		if (!methodEndpoints) {
 			methodEndpoints = [];
 
-			endpoints.set(method, methodEndpoints);
+			endpoints[method] = methodEndpoints;
 		}
 
 		const finalPath =
@@ -108,53 +102,34 @@ const step = (
 		});
 	}
 
-	return {
-		chain,
-		path,
-	};
+	return { chain, path };
 };
 
-const getDispatcher = (app: App, endpoint: Endpoint) => {
+const getDispatcher = (app: Cudenix, endpoint: Endpoint) => {
+	let dispatcher = (request: Request, match?: RegExpExecArray) =>
+		app.endpoint(endpoint, match?.[2] ?? endpoint.path, request, match);
+
 	const safeStatic =
 		endpoint.route.static &&
 		endpoint.use === 0 &&
 		endpoint.chain.length === 0;
 
-	const fallback = (request: Request, match?: RegExpExecArray) => {
-		return app.endpoint(
-			endpoint,
-			match?.[2] ?? endpoint.path,
-			request,
-			match,
-		);
-	};
-
-	let dispatcher = fallback;
-
 	if (endpoint.jit && !safeStatic) {
 		dispatcher = compileEndpointFetch(endpoint, app);
-
-		endpoint.compiled = dispatcher;
 	}
 
-	if (safeStatic && endpoint.route.literal) {
+	if (safeStatic) {
 		return {
 			constant: dispatcher(undefined as any) as Response,
 			dispatcher,
 		};
 	}
 
-	if (safeStatic) {
-		dispatcher = memoizeRequest(dispatcher);
-	}
-
-	return {
-		dispatcher,
-	};
+	return { dispatcher };
 };
 
-export const compile = (app: App) => {
-	const endpoints = new Map<string, Endpoint[]>();
+export const compile = (app: Cudenix) => {
+	const endpoints = new Empty() as Record<string, Endpoint[]>;
 
 	step(endpoints, app.memory.module as AnyModule, {
 		bits: 0,
@@ -162,19 +137,19 @@ export const compile = (app: App) => {
 		path: "",
 	});
 
-	app.memory.endpoints = endpoints;
+	for (const method in endpoints) {
+		const methodEndpoints = endpoints[method];
 
-	for (const [method, methodEndpoints] of endpoints) {
-		if (methodEndpoints.length === 0) {
+		if (!methodEndpoints || methodEndpoints.length === 0) {
 			continue;
 		}
 
 		const routes = app.routes;
 
-		const dynamicEndpoints = [] as Endpoint[];
 		const methodRegexps = [] as string[];
+		const regexpEndpoints = [] as Endpoint[];
 
-		let groupOffset = 3;
+		let matchOffset = 3;
 
 		for (let i = 0; i < methodEndpoints.length; i++) {
 			const methodEndpoint = methodEndpoints[i];
@@ -183,52 +158,46 @@ export const compile = (app: App) => {
 				continue;
 			}
 
-			const { constant, dispatcher } = getDispatcher(app, methodEndpoint);
+			const { restKeys, paramKeys, pattern } = pathToRegexp(
+				methodEndpoint.path,
+			);
 
 			methodEndpoint.jit = methodEndpoint.route.jit ?? app.jit;
+			methodEndpoint.matchOffset = matchOffset;
+			methodEndpoint.paramKeys = paramKeys;
+			methodEndpoint.restKeys = restKeys;
+
+			matchOffset += 1 + paramKeys.length;
+
+			methodRegexps.push(pattern);
+			regexpEndpoints.push(methodEndpoint);
 
 			if (
-				methodEndpoint.path.indexOf("?") !== -1 ||
-				methodEndpoint.path.indexOf("...") !== -1
+				methodEndpoint.path.indexOf("?") === -1 &&
+				methodEndpoint.path.indexOf("...") === -1
 			) {
-				const { restKeys, paramKeys, pattern } = pathToRegexp(
-					methodEndpoint.path,
-					{
-						capture: true,
-					},
+				const { constant, dispatcher } = getDispatcher(
+					app,
+					methodEndpoint,
 				);
 
-				methodEndpoint.restKeys = restKeys;
-				methodEndpoint.markerIndex = groupOffset;
-				methodEndpoint.paramKeys = paramKeys;
+				if (!(methodEndpoint.path in routes)) {
+					routes[methodEndpoint.path] =
+						new Empty() as (typeof routes)[string];
+				}
 
-				groupOffset += 1 + paramKeys.length;
-
-				methodRegexps.push(pattern);
-				dynamicEndpoints.push(methodEndpoint);
-
-				methodEndpoint.compiled = constant
-					? () => {
-							return constant.clone();
-						}
-					: dispatcher;
-
-				continue;
+				routes[methodEndpoint.path]![method] =
+					constant ??
+					(dispatcher as Bun.Serve.Handler<any, any, any>);
 			}
-
-			routes[methodEndpoint.path] ??=
-				new Empty() as (typeof routes)[string];
-
-			routes[methodEndpoint.path]![method] =
-				constant ?? (dispatcher as Bun.Serve.Handler<any, any, any>);
 		}
 
-		if (dynamicEndpoints.length === 0) {
+		if (regexpEndpoints.length === 0) {
 			continue;
 		}
 
 		app.methods.method = {
-			endpoints: dynamicEndpoints,
+			endpoints: regexpEndpoints,
 			regexp: new RegExp(
 				`^(https?:\\/\\/)[^\\s\\/]+(${methodRegexps.join("|")})(?![^?#])`,
 			),
